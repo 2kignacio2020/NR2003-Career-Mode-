21cb8f5ad67d80110d041408921f2b44aa24d346
2kignacio2020 <131908546+2kignacio2020@users.noreply.github.com>
2023-04-28 17:26:41 +0200

    Update main.py
    Now you are able to put teams name manually, meanwhile we find a way to make a db

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
-# Importamos la librería para trabajar con archivos
-import os
+import random
 
-# Definimos la función que agrupa los equipos y pilotos y asigna la reputación
-def generar_lista(nombre_archivo):
+# Creamos una lista vacía para almacenar los equipos
+equipos = []
+
+# Pedimos el nombre del usuario
+nombre = input("Hola, ¿cómo te llamas? ")
+
+# Pedimos los equipos, números y reputación al usuario
+while True:
+    # Pedimos los datos del equipo
+    equipo = input("Ingresa el nombre del equipo o escribe 'Estoy harto' para salir: ")
     
-    # Abrimos el archivo
-    with open(nombre_archivo, 'r') as archivo:
-        # Leemos todas las líneas del archivo
-        lineas = archivo.readlines()
-        
-        # Creamos un diccionario para almacenar los equipos y pilotos
-        equipos_pilotos = {}
-        
-        # Recorremos cada línea del archivo
-        for linea in lineas:
-            # Separamos la línea en las diferentes partes (equipo, piloto, número de coches)
-            partes = linea.strip().split(',')
-            
-            # Si el equipo ya existe en el diccionario, agregamos el piloto y el número de coches
-            if partes[0] in equipos_pilotos:
-                equipos_pilotos[partes[0]]['pilotos'].append(partes[1])
-                equipos_pilotos[partes[0]]['coches'] += int(partes[2])
-            # Si el equipo no existe, creamos una nueva entrada en el diccionario
-            else:
-                equipos_pilotos[partes[0]] = {'pilotos': [partes[1]], 'coches': int(partes[2])}
-        
-        # Creamos una lista con los equipos y pilotos, y les asignamos la reputación basada en el número de coches
-        lista_final = []
-        for equipo, datos in equipos_pilotos.items():
-            reputacion = datos['coches'] * 100
-            for piloto in datos['pilotos']:
-                lista_final.append({'equipo': equipo, 'piloto': piloto, 'reputacion': reputacion})
+    # Si el usuario escribe 'Estoy harto', salimos del ciclo
+    if equipo == "Estoy harto":
+        break
     
-    # Retornamos la lista final ordenada por equipo y piloto
-    return sorted(lista_final, key=lambda x: (x['equipo'], x['piloto']))
+    numero_coches = int(input("Ingresa el número de coches del equipo: "))
+    reputacion = int(input("Ingresa la reputación del equipo (entre 1 y 100): "))
+    
+    # Agregamos los datos del equipo a la lista
+    equipos.append({'equipo': equipo, 'coches': numero_coches, 'reputacion': reputacion})
 
-# Pedimos al usuario que ingrese el nombre del archivo
-nombre_archivo = input("Ingrese el nombre del archivo: ")
+# Generamos 3 ofertas aleatorias a partir de los equipos ingresados
+ofertas = []
+for i in range(3):
+    # Seleccionamos un equipo aleatorio de la lista
+    equipo = random.choice(equipos)
+    
+    # Calculamos el precio base de la oferta
+    precio_base = equipo['coches'] * 500
+    
+    # Aplicamos un descuento aleatorio entre el 10% y el 20%
+    descuento = random.randint(10, 20) / 100
+    precio_oferta = precio_base * (1 - descuento)
+    
+    # Agregamos la oferta a la lista
+    ofertas.append({'equipo': equipo['equipo'], 'precio_base': precio_base, 'descuento': descuento, 'precio_oferta': precio_oferta})
 
-# Verificamos si el archivo existe
-if os.path.exists(nombre_archivo):
-    # Si existe, generamos la lista
-    lista_final = generar_lista(nombre_archivo)
-    # Imprimimos la lista final
-    for item in lista_final:
-        print(item)
-else:
-    # Si no existe, mostramos un mensaje de error
-    print("El archivo no existe.")
+# Mostramos las ofertas generadas
+print("Estas son las ofertas que hemos generado para ti, {}:".format(nombre))
+for oferta in ofertas:
+    print("Oferta para el equipo {}: precio base ${}, descuento del {}% (ahorro de ${}), precio de oferta ${}".format(oferta['equipo'], oferta['precio_base'], oferta['descuento']*100, oferta['precio_base']*oferta['descuento'], oferta['precio_oferta']))
+
